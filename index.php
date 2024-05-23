de6408eb65613fc9142519659ad1ca511ae36356
Maria024code <166326347+Maria024code@users.noreply.github.com>
2024-05-23 14:32:48 +0300

    My Practice

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+echo "My name is Mary Mutobu"<br>;
+print"I was born in Nairobi in 1980"<br>;
+print "Now i am 44 years old"<br>;
+
+
+
+
+?>
+
+
+</body>
+</html>
+
+
+
